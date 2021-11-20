9a38feef3f77fd5f7259968cd046574a1a00aebd
MattyCi <mjciaravino@gmail.com>
2021-11-20 15:36:58 -0500

    use properties files for popupMessage parameter (forgot to add files)

---
+++ src/main/java/PropertiesSelection/PopupMessages.java
@@ -0,0 +1,15 @@
+package PropertiesSelection;
+
+public class PopupMessages {
+	
+	public static final String SEASON_CREATE_CONFIRMATION = "SEASON_CREATE_CONFIRMATION";
+	public static final String SEASON_DELETE_CONFIRMATION = "SEASON_DELETE_CONFIRMATION";
+	public static final String SEASON_EDIT_CONFIRMATION = "SEASON_EDIT_CONFIRMATION";
+	
+	public static final String ROUND_DELETE_CONFIRMATION = "ROUND_DELETE_CONFIRMATION";
+	
+	public static final String ACCOUNT_REDEEM_SUCCESS = "ACCOUNT_REDEEM_SUCCESS";
+	public static final String ACCOUNT_DELETE_SUCCESS = "ACCOUNT_DELETE_SUCCESS";
+	public static final String ACCOUNT_UPDATE_SUCCESS = "ACCOUNT_UPDATE_SUCCESS";
+	
+}
